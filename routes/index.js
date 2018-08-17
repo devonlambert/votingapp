216ee524c41a2f1d7731a9e229494f93a19399ca
Devon Lambert <devon@realityknights.com>
2018-08-16 22:52:19 -0500

    Parses pollData from JSON file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+/* GET the JSON data */
+var pollData = JSON.parse(fs.readFileSync(__dirname + '/../public/data/polldata.json'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Voting App',
+    pollData: pollData
+  });
 });
 
 module.exports = router;
